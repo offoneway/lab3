9b85ce460043723d4588d101fce700a2374f588d
chinatip7799 <85872909+chinatip7799@users.noreply.github.com>
2022-01-15 01:59:27 +0700

    lll

---
+++ backend/entity/schema.go
@@ -0,0 +1,45 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Customer struct {
+	gorm.Model
+	Name           string
+	ID_Customer    string `gorm:"uniqueIndex"`
+	Password       string
+	RepairRequests []RepairRequest `gorm:"foreignKey:CustomerID"`
+}
+type RepairType struct {
+	gorm.Model
+	Name           string
+	RepairRequests []RepairRequest `gorm:"foreignKey:CustomerID"`
+}
+type Urgency struct {
+	gorm.Model
+	Name           string
+	RepairRequests []RepairRequest `gorm:"foreignKey:CustomerID"`
+}
+type RepairRequest struct {
+	gorm.Model
+
+	Device      string
+	lifetime    uint
+	issue       string
+	RequestDate time.Time
+
+	CustomerID *uint
+	Customer   Customer `gorm:"references:id"`
+
+	RepairTypeID *uint
+	RepairType   RepairType `gorm:"references:id"`
+
+	UrgencyID *uint
+	Urgency   Urgency `gorm:"references:id"`
+
+	RepairRequstID *uint
+	RepairRequest  []RepairRequest `gorm:"references:id"`
+}
---
+++ backend/entity/setup.go
@@ -0,0 +1,21 @@
+package entity
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func DB() *gorm.DB {
+	return db
+}
+
+func SetupDatabase() {
+	database, err := gorm.Open(sqlite.Open("schema.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	database.AutoMigrate()
+	db = database
+}
